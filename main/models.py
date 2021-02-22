95439c7cc78221f447754a98129bda057ddc8f5d
Wei Xing <weixingp5@gmail.com>
2021-02-22 13:24:18 +0800

    added question and answer model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Question(models.Model):
+    question = models.TextField(max_length=1000)
+    # world_id = models.ForeignKey(World, on_delete=models.CASCADE, related_name="question_world_fk")
+    DIFFICULTY_CHOICES = (
+        ("1", "Easy"),
+        ("2", "Normal"),
+        ("3", "Hard"),
+    )
+    difficulty = models.CharField(
+        max_length=1,
+        choices=DIFFICULTY_CHOICES,
+    )
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="question_world_fk")
+    time_created = models.DateTimeField(auto_now=True)
+
+    # Object name for display in admin panel
+    def __str__(self):
+        return self.question
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="question_fk")
+    answer = models.CharField(max_length=256)
+    is_correct = models.BooleanField(default=False)
+
+    # Object name for display in admin panel
+    def __str__(self):
+        if self.is_correct:
+            return "Correct"
+        else:
+            return "Incorrect"
