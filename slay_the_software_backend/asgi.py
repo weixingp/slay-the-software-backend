8d8e89e3a3212073dbe701f203e6ed114594ed05
Wei Xing <weixingp5@gmail.com>
2021-02-22 12:57:58 +0800

    Initial commit

---
+++ slay_the_software_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for slay_the_software_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slay_the_software_backend.settings')
+
+application = get_asgi_application()
